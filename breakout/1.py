efe90e81ab187782e616e39304ecbf3f1f343396
Chathula Magage <Chathula.Magage@tlgcohort.com>
2021-08-09 01:07:14 +0000

    game update

---
+++ breakout/1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def namechanger(n):
+    
+    print(n + " is a wild and crazy guy")
+    # code goes here
+names= ["Jauric","Chas","Chathula","Bikash"]
+for name in names:
+    namechanger(name) #make the name print out from here instead of the function
---
+++ challenges/fizbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import pandas as pd
+
+def main():
+    csv_file = 'numfile.txt'
+
+    fizbuzz = pd.read_csv(numfile.txt)
+    print(fizbuzz.head())
+
+
+
+if __name__ == "__main__":
+    main()
